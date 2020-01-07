8852b21d60e7e02188dd6553d744eac5994d42c6
lvnam96 <lvnam96@gmail.com>
2020-01-07 11:50:55 +0700

    Remove eslint rule: max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     'no-use-before-define': ['warn', 'nofunc'],
     'no-unused-vars': 'warn',
     'no-var': 'warn',
-    'max-len': 'warn',
+    'max-len': 'off',
     'no-underscore-dangle': 'off',
     'linebreak-style': 'off',
     'max-classes-per-file': 'off'
